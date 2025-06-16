0d3f2e1184db7cca3c5a70f3e412f21b7bd02c0d
nima64 <kennethe407@gmail.com>
2025-06-16 14:51:04 -0700

    created signup page

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -16,7 +16,7 @@ interface Signup1Props {
   loginUrl?: string;
 }
 
-export const SignupForm = ({
+export default function SignupForm({
   heading,
   logo = {
     url: "https://www.shadcnblocks.com",
@@ -28,13 +28,13 @@ export const SignupForm = ({
   signupText = "Create an account",
   loginText = "Already have an account?",
   loginUrl = "#",
-}: Signup1Props) => {
+}: Signup1Props) {
   return (
-      <div className="flex h-full items-center justify-center">
-        <div className="flex w-full max-w-sm flex-col items-center gap-y-8 rounded-md border border-muted bg-white px-6 py-12 shadow-md">
-          <div className="flex flex-col items-center gap-y-2">
-            {/* Logo */}
-            {/* <div className="flex items-center gap-1 lg:justify-start">
+    <div className="flex h-full items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col items-center gap-y-8 rounded-md border border-muted bg-white px-6 py-12 shadow-md">
+        <div className="flex flex-col items-center gap-y-2">
+          {/* Logo */}
+          {/* <div className="flex items-center gap-1 lg:justify-start">
               <a href={logo.url}>
                 <img
                   src={logo.src}
@@ -44,48 +44,48 @@ export const SignupForm = ({
                 />
               </a>
             </div> */}
-            {heading && <h1 className="text-3xl font-semibold">{heading}</h1>}
-          </div>
-          <div className="flex w-full flex-col gap-8">
+          {heading && <h1 className="text-3xl font-semibold">{heading}</h1>}
+        </div>
+        <div className="flex w-full flex-col gap-8">
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <Input
+                type="email"
+                placeholder="Email"
+                required
+                className="bg-white"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Input
+                type="password"
+                placeholder="Password"
+                required
+                className="bg-white"
+              />
+            </div>
             <div className="flex flex-col gap-4">
-              <div className="flex flex-col gap-2">
-                <Input
-                  type="email"
-                  placeholder="Email"
-                  required
-                  className="bg-white"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  required
-                  className="bg-white"
-                />
-              </div>
-              <div className="flex flex-col gap-4">
-                <Button type="submit" className="mt-2 w-full">
-                  {signupText}
-                </Button>
-                <Button variant="outline" className="w-full">
-                  <FcGoogle className="mr-2 size-5" />
-                  {googleText}
-                </Button>
-              </div>
+              <Button type="submit" className="mt-2 w-full">
+                {signupText}
+              </Button>
+              {/* <Button variant="outline" className="w-full">
+                <FcGoogle className="mr-2 size-5" />
+                {googleText}
+              </Button> */}
             </div>
           </div>
-          <div className="flex justify-center gap-1 text-sm text-muted-foreground">
-            <p>{loginText}</p>
-            <a
-              href={loginUrl}
-              className="font-medium text-primary hover:underline"
-            >
-              Login
-            </a>
-          </div>
+        </div>
+        <div className="flex justify-center gap-1 text-sm text-muted-foreground">
+          <p>{loginText}</p>
+          <a
+            href={loginUrl}
+            className="font-medium text-primary hover:underline"
+          >
+            Login
+          </a>
         </div>
       </div>
+    </div>
   );
 };
 
